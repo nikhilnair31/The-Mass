822090a041dd7718b718eb22a9b96d82b4577a50
Nikhil Nair <niknair31898@gmail.com>
2025-01-20 16:26:56 -0500

    Added some metal tex.Improved the top vent.  Added flicker to torch.

--- Assets/Axtras/Scripts/Interactable/Interactable_Torch.cs
+++ Assets/Axtras/Scripts/Interactable/Interactable_Torch.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Interactable_Torch : Controller_Pickable 
@@ -6,6 +7,12 @@ public class Interactable_Torch : Controller_Pickable
     [Header("Light Settings")]
     [SerializeField] private bool isOn = true;
     [SerializeField] private Light[] lightsList;
+
+    [Header("Flicker Settings")]
+    [SerializeField] private float minFlickerInterval = 0.05f;
+    [SerializeField] private float maxFlickerInterval = 0.3f;
+    [SerializeField] private float flickerChance = 0.3f;
+    private Coroutine flickerCoroutine;
 
     [Header("Audio Settings")]
     [SerializeField] private AudioClip[] switchClips;
@@ -17,6 +24,7 @@ public class Interactable_Torch : Controller_Pickable
     private void InitializeSwitchState(bool initialState) {
         isOn = initialState;
         LightsControl();
+        StartCoroutine(FlickerLights());
     }
 
     public void ToggleSwitch() {
@@ -24,9 +32,22 @@ public class Interactable_Torch : Controller_Pickable
         LightsControl();
         Helper.Instance.PlayRandAudio(audioSource, switchClips);
     }
+
     private void LightsControl() {
         foreach (var light in lightsList) {
             light.enabled = isOn;
+        }
+    }
+
+    private IEnumerator FlickerLights() {
+        while (true) {
+            float flickerInterval = Random.Range(minFlickerInterval, maxFlickerInterval);
+
+            foreach (var light in lightsList) {
+                light.enabled = Random.value > flickerChance && isOn;
+            }
+
+            yield return new WaitForSeconds(flickerInterval);
         }
     }
 }
